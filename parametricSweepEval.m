32201b94434fc2d4c9c810134b74e64d4ba5e4f7
Matt Williams <72998147+Matt-Williams-198@users.noreply.github.com>
2024-03-14 15:52:11 +0000

    Add files via upload

--- parametricSweepEval.m
+++ parametricSweepEval.m
@@ -8,14 +8,35 @@ for i = 1:size(StoredDataS,1)
         PDRofAlpha(i,j) = sum(StoredDataR(i,j,:))/ModelLength;
     end
 end
+alphaOutputs = zeros(size(PDIofAlpha,1),3);
+for i = 1 : size(PDIofAlpha)
+    alphaOutputs(i,1) = sum(PDSofAlpha(:,i));
+    alphaOutputs(i,2) = sum(PDIofAlpha(:,i));
+    alphaOutputs(i,3) = sum(PDRofAlpha(:,i));
+end
 t = linspace(0,tfinal,100);
 plot(t,squeeze(PDIofAlpha(1,:)),'b');
 hold on
 plot(t,squeeze(PDIofAlpha(10,:)),'r');
 plot(t,squeeze(PDIofAlpha(20,:)),'k');
+title('Population Density over time against Alpha')
 xlabel('Time','FontSize',18,'interpreter','latex')
 ylabel('Mean P.D. of $I$','FontSize',18,'interpreter','latex')
 legend('alpha = 0.005','alpha = 0.05','alpha = 0.1')
 set(gca,'fontsize',17)
 ylim = max(Splot);
 hold off
+
+TotalPop = sum(alphaOutputs(1,:));
+alphaOutputs = alphaOutputs *10./TotalPop;
+localx = linspace(0.1,1,20);
+
+figure
+plot(localx,alphaOutputs(:,2));
+hold on
+title('Total population against Alpha')
+xlabel('Alpha')
+ylabel('Total Infected Population')
+plot(localx,alphaOutputs(:,1));
+plot(localx,alphaOutputs(:,3));
+legend('Infected', 'Susceptible','Recovered')
